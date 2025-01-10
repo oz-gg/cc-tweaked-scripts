7978ac973187a9655d3c9e3790f161007f2b4f3d
oz-gg <160617598+oz-gg@users.noreply.github.com>
2025-01-11 02:39:22 +0300

    tag filtering

--- geoScanTest.lua
+++ geoScanTest.lua
@@ -1,6 +1,17 @@
 scanner = peripheral.find("geoScanner")
+print("enter tag to scan for")
+tag = read()
 result = scanner.scan(10)
 
+function findInTable(v, table)
+	for i, x in ipairs(table) do
+		if x == v then return true end
+	end
+	return false
+end
+
 for i, v in pairs(result) do
-	print(v.name)
+	if findInTable(tag, v.tags) then
+		print(v.name)
+	end
 end
