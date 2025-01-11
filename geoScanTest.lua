7eb9b2f6f09eeba9dd5580612718ace6c0eb4598
oz-gg <160617598+oz-gg@users.noreply.github.com>
2025-01-11 22:21:10 +0300

    added radar function

--- geoScanTest.lua
+++ geoScanTest.lua
@@ -1,4 +1,5 @@
 FILTER_TAG = "forge:ores"
+SCAN_RADIUS = 8
 
 function stringInTable(v, t)
 	for i, x in ipairs(t) do
@@ -7,7 +8,7 @@ function stringInTable(v, t)
 	return false
 end
 
-function analyze(tag)
+function analyze()
 	term.clear()
 	term.setCursorPos(1, 1)
 	result, err = scanner.chunkAnalyze()
@@ -20,13 +21,93 @@ function analyze(tag)
 	read()
 end
 
+function radar()
+	local w, h = term.getSize()
+	local sx, sy = math.floor(w / 2), math.floor(h / 2)
+
+	continueRadar = true
+	elevationStep = false
+	while continueRadar do
+		--reset color
+		paintutils.drawPixel(1, 1, colors.black)
+		term.clear()
+
+		--print north
+		term.setCursor(sx, 2)
+		print("N")
+
+		--print south
+		term.setCursor(sx, h)
+		print("S")
+
+		--print west
+		term.setCursor(1, sy)
+		print("W")
+
+		--print east
+		term.setCursor(w, sy)
+		print("E")
+
+		--center
+		paintutils.drawPixel(sx, sy, colors.gray)
+
+		local scanResult, err = scanner.scan(SCAN_RADIUS)
+
+		if not data then
+			paintutils.drawPixel(3, 3, colors.red)
+			term.setCursorPos(3, 3)
+			print("Scan Error: "..err.."\n")
+		else
+			local noOreFound = true
+			for i, data in ipairs(scanResult) do
+				if data.name and data.x and data.x and data.z and data.tags then
+					if stringInTable(FILTER_TAG, data.tags) then
+						local oreX = sx + data.x
+						local oreY = sy + data.z
+
+						local outOfRange = (oreX < 1) or (oreX > w) or (oreY < 1) or (oreY > h)
+
+						if not outOfRange then
+							local pixelColor = colors.cyan
+							if data.y > 0 then
+								pixelColor = colors.lime
+							else
+								pixelColor = colors.orange
+							end
+
+							paintutils.drawPixel(oreX, oreY, pixelColor)
+
+							term.setCursorPos(oreX, oreY)
+							if elevationStep then
+								print(math.abs(data.y))
+							else
+								if data.y > 0 then
+									print("+")
+								elseif data.y < 0 then
+									print("-")
+								else
+									print("=")
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+		elevationStep = not elevationStep
+	end
+end
+
 scanner = peripheral.find("geoScanner")
 
 continueOperation = true
 while continueOperation do
 	term.clear()
 	term.setCursorPos(1, 1)
+	print("currently looking for:\n"..FILTER_TAG.."\n")
 	print("[a] analyze chunk")
+	print("[r] radar")
+	print("[s] change scan radius")
 	print("[x] change target tag")
 	print("[q] quit")
 
@@ -34,6 +115,13 @@ while continueOperation do
 
 	if key == keys.a then
 		analyze()
+	elseif key == keys.r then
+		radar()
+	elseif key == keys.s then
+		term.clear()
+		term.setCursorPos(1, 1)
+		print("change scan radius ("..SCAN_RADIUS..") to (1-9) :")
+		SCAN_RADIUS = math.clamp(1, 9, int(read())) or 8
 	elseif key == keys.x then
 		term.clear()
 		term.setCursorPos(1, 1)
